10ebc0bed71f4ecbffa235234acdfd8fed130608
lvalle <Luis.valle@mercadolibre.com.co>
2022-05-20 16:25:36 -0500

    gorutine

---
+++ eje14-gorutine/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+)
+
+func main() {
+	//w := &sync.WaitGroup{}
+	go deletrearPalabra("Perro camina despacio")
+	go imprimirNumeros(12)
+	time.Sleep(5000 * time.Millisecond)
+}
+func deletrearPalabra(palabra string) {
+	for _, l := range strings.Split(palabra, "") {
+		log.Println(l)
+		time.Sleep(400 * time.Millisecond)
+	}
+}
+func imprimirNumeros(numeros int) {
+	fmt.Println("Numeros")
+	for i := 0; i < numeros; i++ {
+		log.Println(i)
+		time.Sleep(400 * time.Millisecond)
+	}
+}
